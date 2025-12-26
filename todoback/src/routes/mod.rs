55c436c97116436bc20cdc6b74e86d456fd10f9e
DuncanCos <foxlolmen99@gmail.com>
2025-12-26 23:43:15 +0100

    backend changes cors

--- todoback/src/routes/mod.rs
+++ todoback/src/routes/mod.rs
@@ -24,7 +24,6 @@ pub fn routing(pool: PgPool) -> Router {
             HeaderValue::from_static("http://localhost:5173"),
             HeaderValue::from_static("http://localhost:3000"),
             HeaderValue::from_static("http://127.0.0.1:5173"),
-            HeaderValue::from_static("https://api.todo.dunkan.xyz"),
             HeaderValue::from_static("https://todo.dunkan.xyz"),
         ])
         .allow_methods(vec![
